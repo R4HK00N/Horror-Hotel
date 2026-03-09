7d11bd834e7c93b13aec5018f8e1886294ebde5e
R4HK00N <robert.eddy.beltman@gmail.com>
2026-03-09 13:12:37 +0100

    flashlight revamp + scene push

--- Horror Hotel/Assets/Scripts/Flashlight.cs
+++ Horror Hotel/Assets/Scripts/Flashlight.cs
@@ -1,16 +1,78 @@
+using TMPro;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
     [SerializeField] Light flashlight;
-    bool isOn = false;
+    [SerializeField] float maxBattery;
+    [SerializeField] float batteryIncrease;
+    [SerializeField] float batteryDecrease;
+    [SerializeField] float cooldownTime;
 
+    [Header("test")]
+    [SerializeField] TMP_Text testBatteryText;
+
+    float currentBattery;
+    float currentCooldown;
+    bool isOn = false;
+    bool batteryFull = true;
+    bool onCooldown = false;
+
+    private void Start()
+    {
+        currentBattery = maxBattery;
+        currentCooldown = cooldownTime;
+        flashlight.enabled = isOn;
+    }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && flashlight)
+        if (Input.GetMouseButtonDown(1) && flashlight && !onCooldown)
         {
             isOn = !isOn;
             flashlight.enabled = isOn;
+            onCooldown = true;
+        }
+        if (onCooldown)
+        {
+            FlashlightCooldown();
+        }
+        FlashlightBattery();
+
+        testBatteryText.text = currentBattery.ToString("N0");
+    }
+    void FlashlightBattery()
+    {
+        if (isOn)
+        {
+            if (currentBattery >= 0)
+            {
+                currentBattery -= batteryDecrease * Time.deltaTime;
+            }
+            else
+            {
+                isOn = false;
+                flashlight.enabled = isOn;
+            }
+        }
+        if (!isOn)
+        {
+            if (currentBattery < maxBattery)
+            {
+                currentBattery += batteryIncrease * Time.deltaTime;
+            }
+        }
+    }
+    void FlashlightCooldown()
+    {
+        if(currentCooldown > 0)
+        {
+            print("onCooldown");
+            currentCooldown -= cooldownTime * Time.deltaTime;
+        }
+        else
+        {
+            onCooldown = false;
+            currentCooldown = cooldownTime;
         }
     }
 }
